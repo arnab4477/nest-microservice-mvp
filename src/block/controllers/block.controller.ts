20d0b5741e531e3d08a0f6b8d5226109127ffbf3
arnab4477 <arnab@bhumio.com>
2024-08-10 06:44:43 +0530

    refactor: block controller

--- src/block/controllers/block.controller.ts
+++ src/block/controllers/block.controller.ts
@@ -1,5 +1,5 @@
 import { Request } from 'express';
-import { Body, Controller, InternalServerErrorException, Put, Req } from '@nestjs/common';
+import { Body, Controller, Put, Req } from '@nestjs/common';
 
 import { BlockOrUnblockUserDto } from 'block/dto';
 import { BlockService } from '../services/block.service';
@@ -10,14 +10,10 @@ export class BlockController {
 
   @Put('/')
   block(@Body() data: BlockOrUnblockUserDto, @Req() req: Request) {
-    const blockerId = req.headers['user_id'] as string;
-
-    if (!blockerId) {
-      // This should never happen
-      // Send the error info to Sentry or another monitoring service
-      throw new InternalServerErrorException();
-    }
-
-    return this.blockService.block({ blockerId, blockedId: data.blockedId, block: data.block });
+    return this.blockService.block({
+      blockerId: req.headers['user_id'] as string,
+      blockedId: data.blockedId,
+      block: data.block,
+    });
   }
 }
